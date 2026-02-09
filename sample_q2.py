32e709f3cfd6d17f0446c06c1ae489e3e78c6699
mysticmahak <sahumahak52@gmail.com>
2026-02-09 14:13:57 +0530

    check part b

--- sample_q2.py
+++ sample_q2.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+start = "But your manual KV path does not exactly match Flash Attention semantics. This can cause tiny numerical differences that compound during generation. Not the main issue — but worth knowing." # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 10 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
